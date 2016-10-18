76854e1db66cf9214b5ce5db92390878d135c06d
Ryan Westlake <ryancwestlake@gmail.com>
2016-10-17 23:19:34 -0600

    removed pseudo code in index.js

--- app/index.js
+++ app/index.js
@@ -1,9 +1,2 @@
 require('../app/components/app.jsx')
 require('../public/styles');
-
-//components need for app
-//app
-//input field and button - changes state
-//current time summary
-  //list item
-//week summary
